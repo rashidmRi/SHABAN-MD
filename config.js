deedc9faa68cdb34a111eb0f76ec67fcc48cd07f
rashidmRi <rashidmari302@gmail.com>
2025-09-03 08:39:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return text === fault ? false : false;
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
